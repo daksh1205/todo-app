b99034ae744102ba70b1636c6b763b6a6f53e616
daksh <dakshyt1205@gmail.com>
2024-01-26 12:09:14 +0530

    first commit

---
+++ lib/screens/add_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AddTodoPage extends StatefulWidget {
+  const AddTodoPage({super.key});
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add Todo',
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextField(
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          TextField(
+            decoration: InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Submit'),
+          )
+        ],
+      ),
+    );
+  }
+  void submitData() {
+    // Get the data from FORM
+    // Submit the data to the server
+    // show success or fail message based on status
+  }
+}
